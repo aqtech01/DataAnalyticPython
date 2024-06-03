3c15e4a151d87fac7a6fb451acce723d698ef9aa
AQTECH <abdulqadeer0120@gmail.com>
2024-06-03 15:39:56 +0100

    till logical operators

---
+++ index.py
@@ -0,0 +1,48 @@
+# name = "Abdul Qadeer"
+# age = 23
+# address = "Pakistan"
+#
+# print(name)
+# print(age)
+# print(address)
+#
+# a = 10
+# b = 20
+# use temp variable
+
+# temp = a
+# give values to b
+# a = b
+# temp value to b
+# b = temp
+# print(b)
+# print(a)
+#
+# x = 20
+# print(type(x))
+# change the value to float
+# x = float(x)
+# print(type(x))
+
+
+# take details from user
+
+#
+# name = input("Enter Your name : ")
+# grade = float(input("Enter Your Grade: "))
+# dob = input("Enter DOB : ")
+# email = input("Enter Your email")
+# print("Student Card Details")
+# print(f"Welcome {name}, Your grade is {grade}, DOB is {dob} and your email is {email}")
+#
+
+
+# get user input in int and then convert into float
+
+
+a = int(input("Enter Your Value: "))
+print(a)
+print(f"Data Type before Conversion {type(a)}")
+a = float(a)
+print(a)
+print(f"Data Type after Conversion {type(a)}")
---
+++ operator.py
@@ -0,0 +1,46 @@
+# # Operators
+#
+# # Arithmatic OOperators
+#
+# # +, -, *, / , **, // , %
+# a = 10
+# b = 20
+# # Plus Operators
+# print(a+b)
+# # Subtract Operators
+#
+# print(a-b)
+# # Multiply operator
+# print(a*b)
+# # Division Operators
+# print(a/b)
+# # Flor Division
+# print(a//b)
+# # Modulus OOperators
+# print(a % b)
+#  # Exponential Operators
+# print(a**b)
+
+
+# Comparison OOperators
+#
+# print(10 > 20)
+# print(10 < 20)
+# print(10 <= 20)
+# print(10 >= 20)
+# print(10 == 20)
+# print(10 != 20)
+
+
+# Logical Operators
+#
+# and Operator
+# print(True and False)
+# or Operator
+
+# print(True or False)
+# not operator
+# print(not False)
+#
+
+# Assignment Operators
